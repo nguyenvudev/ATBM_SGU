f385771ba1f11defabe6144469d133afa3cb37f3
XnCanh <xuanc3879@gmail.com>
2024-11-01 12:43:20 +0700

    icon file + doi mk

--- static/js/inbox-script.js
+++ static/js/inbox-script.js
@@ -1,126 +1,55 @@
+// Đóng mở sidebar
 let sidebar = document.querySelector(".sidebar");
-    let closeBtn = document.querySelector("#btn");
+let closeBtn = document.querySelector("#btn");
 
-    closeBtn.addEventListener("click", () => {
-        sidebar.classList.toggle("open");
-        menuBtnChange();
-    });
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    menuBtnChange();
+});
 
-    function menuBtnChange() {
-        if (sidebar.classList.contains("open")) {
-            closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-        } else {
-            closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
-        }
+function menuBtnChange() {
+    if (sidebar.classList.contains("open")) {
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else {
+        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
     }
-//
-//// Delete icon mail received
-//function toggleDeleteIconReceived() {
-//    let checkboxesChecked = document.querySelectorAll('.email-checkbox-received:checked').length > 0;
-//    let deleteIcon = document.getElementById('delete-icon-1');
-//    deleteIcon.style.display = checkboxesChecked ? 'block' : 'none';
-//    }
-//
-//    document.getElementById('select-all-received').addEventListener('change', function(event) {
-//        let checkboxes = document.querySelectorAll('.email-checkbox-received');
-//        checkboxes.forEach(checkbox => checkbox.checked = event.target.checked);
-//        toggleDeleteIconReceived();
-//    });
-//
-//    document.querySelectorAll('.email-checkbox-received').forEach(checkbox => {
-//        checkbox.addEventListener('change', toggleDeleteIconReceived);
-//    });
-//
-//    toggleDeleteIconReceived();
-function toggleDeleteIconReceived() {
-    let checkboxesChecked = document.querySelectorAll('.email-checkbox-received:checked').length > 0;
-    let deleteIcon = document.getElementById('delete-icon-1');
-    deleteIcon.style.display = checkboxesChecked ? 'block' : 'none';
 }
 
-document.getElementById('select-all-received').addEventListener('change', function(event) {
-    let checkboxes = document.querySelectorAll('.email-checkbox-received');
-    checkboxes.forEach(checkbox => checkbox.checked = event.target.checked);
-    toggleDeleteIconReceived();
-});
-
-document.querySelectorAll('.email-checkbox-received').forEach(checkbox => {
-    checkbox.addEventListener('change', toggleDeleteIconReceived);
-});
-
-toggleDeleteIconReceived();
-
-
-// Delete icon mail send
-function toggleDeleteIconSend() {
-    let checkboxesChecked = document.querySelectorAll('.email-checkbox-send:checked').length > 0;
-    let deleteIcon = document.getElementById('delete-icon-2');
-    deleteIcon.style.display = checkboxesChecked ? 'block' : 'none';
-    }
-
-    document.getElementById('select-all-send').addEventListener('change', function(event) {
-        let checkboxes = document.querySelectorAll('.email-checkbox-send');
-        checkboxes.forEach(checkbox => checkbox.checked = event.target.checked);
-        toggleDeleteIconSend();
-    });
-
-    document.querySelectorAll('.email-checkbox-send').forEach(checkbox => {
-        checkbox.addEventListener('change', toggleDeleteIconSend);
-    });
-
-    toggleDeleteIconSend();
-
-function toggleDeleteIconTrash() {
-    let checkboxesChecked = document.querySelectorAll('.email-checkbox-trash:checked').length > 0;
-    let deleteIcon = document.getElementById('delete-icon-3');
-    deleteIcon.style.display = checkboxesChecked ? 'block' : 'none';
-    }
-
-    document.getElementById('select-all-trash').addEventListener('change', function(event) {
-        let checkboxes = document.querySelectorAll('.email-checkbox-trash');
-        checkboxes.forEach(checkbox => checkbox.checked = event.target.checked);
-        toggleDeleteIconTrash();
-    });
-
-    document.querySelectorAll('.email-checkbox-trash').forEach(checkbox => {
-        checkbox.addEventListener('change', toggleDeleteIconTrash);
-    });
-
-    toggleDeleteIconTrash();
-
-// Show account
+// Hiển thị
 document.addEventListener("DOMContentLoaded", function() {
-    // // Get the elements
-    // const mailCreateBtn = document.querySelector('.mail-create');
+    // Lấy các phần tử
+    const mailCreateBtn = document.querySelector('.mail-create');
     const mailReceivedBtn = document.querySelector('.mail-received');
     const mailSendBtn = document.querySelector('.mail-send');
     const mailTrashBtn = document.querySelector('.mail-trash');
 
-    // const formMailCreate = document.querySelector('.form-create-mail');
+    const formMailCreate = document.querySelector('.form-create-mail');
     const formMailReceived = document.querySelector('.form-mail-received');
     const formMailSend = document.querySelector('.form-mail-send');
     const formMailTrash = document.querySelector('.form-trash-mail');
+    const formRecoverKey = document.querySelector('.form-recover-key');
+    const closeComposeIcon = document.getElementById('close-compose');
+    const closeRecoverKey = document.getElementById('close-change');
+    const btnRecover = document.querySelector(".recover-key button");
+    const btnForm = document.querySelector(".profile-details");
 
-    // Function to remove active class from all forms except the mail-create form
+    let isFormRecover = false;
+    let isFormVisible = false;
+
+    // Chức năng chuyển đổi giữa các form hộp thư
     function resetForms() {
         formMailReceived.classList.remove('active');
         formMailSend.classList.remove('active');
         formMailTrash.classList.remove('active');
     }
 
-    // Show mail-received by default
+    // Hiển thị hộp thư đến mặc định
     formMailReceived.classList.add('active');
-
-    // Add event listeners to buttons
-    // mailCreateBtn.addEventListener('click', function() {
-    //     formMailCreate.classList.toggle('active'); // Toggle instead of add
-    // });
 
     mailReceivedBtn.addEventListener('click', function() {
         resetForms();
         formMailReceived.classList.add('active');
     });
-
     mailSendBtn.addEventListener('click', function() {
         resetForms();
         formMailSend.classList.add('active');
@@ -128,45 +57,56 @@ document.addEventListener("DOMContentLoaded", function() {
     mailTrashBtn.addEventListener('click', function(){
         resetForms();
         formMailTrash.classList.add('active');
-    })
-});
+    });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const formMailCreate = document.querySelector('.form-create-mail');
-    const closeComposeIcon = document.getElementById('close-compose');
+    // Chức năng mở và đóng form Tạo Email
+    mailCreateBtn?.addEventListener('click', function() {
+        formMailCreate.classList.toggle('active');
+    });
 
-    // Hàm để tắt form
     function closeCreateMail() {
-        formMailCreate.classList.remove('active'); // Xóa class active để ẩn form
+        formMailCreate.classList.remove('active');
     }
+    closeComposeIcon?.addEventListener('click', closeCreateMail);
 
-    // Bắt sự kiện khi nhấn vào biểu tượng "x"
-    closeComposeIcon.addEventListener('click', closeCreateMail);
+    // Chức năng mở và đóng form Khôi Phục Mật Khẩu
+    btnRecover?.addEventListener("click", function(e) {
+        e.preventDefault();
+        if (!isFormRecover) {
+            formRecoverKey.style.opacity = 1;
+            formRecoverKey.style.visibility = "visible";
+            formRecoverKey.style.transform = "scale(1)";
+            isFormRecover = true;
+        } else {
+            formRecoverKey.style.opacity = 0;
+            formRecoverKey.style.visibility = "hidden";
+            formRecoverKey.style.transform = "scale(0)";
+            isFormRecover = false;
+        }
+    });
 
-    // Bắt sự kiện khi nhấp ra ngoài form
+    function closeFormRecoverKey() {
+        formRecoverKey.classList.remove('active');
+        formRecoverKey.style.opacity = 0;
+        formRecoverKey.style.visibility = "hidden";
+        formRecoverKey.style.transform = "scale(0)";
+        isFormRecover = false;
+    }
+    closeRecoverKey?.addEventListener('click', closeFormRecoverKey);
+
+    // Đóng form khi nhấp ra ngoài
     document.addEventListener('click', function(event) {
         if (!formMailCreate.contains(event.target) && !event.target.closest('.mail-create')) {
             closeCreateMail();
         }
+        if (!formRecoverKey.contains(event.target) && !event.target.closest('.recover-key')) {
+            closeFormRecoverKey();
+        }
     });
 
-    // Giữ nguyên chức năng mở form từ sidebar
-    const mailCreateBtn = document.querySelector('.mail-create');
-    mailCreateBtn.addEventListener('click', function() {
-        formMailCreate.classList.toggle('active'); // Mở/đóng form
-    });
-});
-
-
-
-
-
-
-// Functionality for account profile toggle
-let isFormVisible = false;
-    const btnForm = document.querySelector(".profile-details");
-    btnForm.addEventListener("click", function () {
-        if (isFormVisible == false) {
+    // Chức năng hiển thị thông tin tài khoản
+    btnForm?.addEventListener("click", function() {
+        if (!isFormVisible) {
             btnForm.classList.add("active");
             isFormVisible = true;
         } else {
@@ -174,38 +114,85 @@ let isFormVisible = false;
             isFormVisible = false;
         }
     });
+});
 
-    let isFormRecover = false;
-    const btnRecover = document.querySelector(".recover-key button");
-    const formRecover = document.querySelector(".form-recover-key");
+// Icon delete thư đến
+function toggleDeleteIconReceived() {
+   let checkboxesChecked = document.querySelectorAll('.email-checkbox-received:checked').length > 0;
+   let deleteIcon = document.getElementById('delete-icon-1');
+   deleteIcon.style.display = checkboxesChecked ? 'block' : 'none';
+}
 
-    btnRecover.addEventListener("click", function(e) {
-        e.preventDefault(); // Ngăn chặn nút gửi form mặc định
-        if (isFormRecover == false) {
-            formRecover.style.opacity = 1;
-            formRecover.style.visibility = "visible";
-            formRecover.style.transform = "scale(1)";
-            isFormRecover = true;
-        } else {
-            formRecover.style.opacity = 0;
-            formRecover.style.visibility = "hidden";
-            formRecover.style.transform = "scale(0)";
-            isFormRecover = false;
-        }
-    });
+document.getElementById('select-all-received').addEventListener('change', function(event) {
+   let checkboxes = document.querySelectorAll('.email-checkbox-received');
+   checkboxes.forEach(checkbox => checkbox.checked = event.target.checked);
+   toggleDeleteIconReceived();
+});
 
+document.querySelectorAll('.email-checkbox-received').forEach(checkbox => {
+   checkbox.addEventListener('change', toggleDeleteIconReceived);
+});
 
-// attachment
-document.getElementById('file-upload').addEventListener('change', function() {
+toggleDeleteIconReceived();
+
+// Icon delete thư đã gửi
+function toggleDeleteIconSend() {
+   let checkboxesChecked = document.querySelectorAll('.email-checkbox-send:checked').length > 0;
+   let deleteIcon = document.getElementById('delete-icon-2');
+   deleteIcon.style.display = checkboxesChecked ? 'block' : 'none';
+}
+
+document.getElementById('select-all-send').addEventListener('change', function(event) {
+    let checkboxes = document.querySelectorAll('.email-checkbox-send');
+    checkboxes.forEach(checkbox => checkbox.checked = event.target.checked);
+    toggleDeleteIconSend();
+});
+
+document.querySelectorAll('.email-checkbox-send').forEach(checkbox => {
+    checkbox.addEventListener('change', toggleDeleteIconSend);
+});
+
+toggleDeleteIconSend();
+
+// Icon delete thùng rác
+function toggleDeleteIconTrash() {
+   let checkboxesChecked = document.querySelectorAll('.email-checkbox-trash:checked').length > 0;
+   let deleteIcon = document.getElementById('delete-icon-3');
+   deleteIcon.style.display = checkboxesChecked ? 'block' : 'none';
+}
+
+document.getElementById('select-all-trash').addEventListener('change', function(event) {
+    let checkboxes = document.querySelectorAll('.email-checkbox-trash');
+    checkboxes.forEach(checkbox => checkbox.checked = event.target.checked);
+    toggleDeleteIconTrash();
+});
+
+document.querySelectorAll('.email-checkbox-trash').forEach(checkbox => {
+    checkbox.addEventListener('change', toggleDeleteIconTrash);
+});
+
+toggleDeleteIconTrash();
+
+// Chức năng soạn thư
+
+//  Cập nhật body
+function updateBody() {
+    // Cập nhật giá trị của trường ẩn 'body' từ nội dung của <div id="main">
+    document.getElementById('hidden-body').value = document.getElementById('main').innerHTML;
+}
+
+// Tệp đính kèm
+let selectedFiles = []; // Khởi tạo mảng để lưu trữ các file đã chọn
+
+document.getElementById('attachment').addEventListener('change', function () {
     const fileList = document.getElementById('file-list');
-    const mainContent = document.getElementById('main');
-    const files = this.files;
+    const files = Array.from(this.files); // Lấy danh sách file đã chọn và chuyển thành mảng
 
-    // Xóa nội dung cũ
-    fileList.innerHTML = '';
-    mainContent.innerHTML = ''; // Xóa nội dung chính trước khi thêm mới
+    // Bổ sung các file mới vào mảng selectedFiles
+    selectedFiles = selectedFiles.concat(files);
 
-    // Tạo dropdown (select element)
+    // Cập nhật danh sách file hiển thị
+    fileList.innerHTML = ''; // Xóa nội dung cũ
     const select = document.createElement('select');
     select.className = 'file-dropdown';
 
@@ -213,39 +200,21 @@ document.getElementById('file-upload').addEventListener('change', function() {
     defaultOption.text = 'Danh sách file';
     select.appendChild(defaultOption);
 
-    if (files.length > 0) {
-        // Lặp qua từng file
-        for (let i = 0; i < files.length; i++) {
-            // Kiểm tra nếu file là ảnh
-            if (files[i].type.startsWith('image/')) {
-                const imgSrc = URL.createObjectURL(files[i]);
-                const imgTag = `<img src="${imgSrc}" width="200" height="200" style="margin-right: 20px; border-radius: 5px; border: 1px solid #ddd;">`;
+    // Lặp qua từng file trong mảng selectedFiles để hiển thị
+    selectedFiles.forEach((file, index) => {
+        const option = document.createElement('option');
+        option.text = file.name;
+        option.value = index; // Lưu trữ chỉ mục của file để xóa nếu cần
+        select.appendChild(option);
+    });
 
-                // Thêm hình ảnh vào nội dung chính
-                mainContent.innerHTML += imgTag; // Thêm vào nội dung chính với thẻ img
+    fileList.appendChild(select);
 
-            } else {
-                // Nếu file không phải ảnh, thêm vào dropdown
-                const option = document.createElement('option');
-                option.text = files[i].name;
-                select.appendChild(option);
-            }
-        }
-
-        // Chỉ hiển thị dropdown nếu có file không phải ảnh
-        if (select.children.length > 1) {
-            fileList.appendChild(select);
-        }
-    } else {
-        const noFileMsg = document.createElement('p');
-        noFileMsg.textContent = 'Không có file nào được chọn.';
-        fileList.appendChild(noFileMsg);
-    }
+    // Xóa các file đã chọn sau khi thêm vào mảng để tránh chọn lại khi người dùng nhấp vào input file
+    this.value = '';
 });
-function updateBody() {
-    // Cập nhật giá trị của trường ẩn 'body' từ nội dung của <div id="main">
-    document.getElementById('hidden-body').value = document.getElementById('main').innerHTML;
-}
+
+///////////////////////////////////
 function moveToTrash() {
     const selectedEmails = document.querySelectorAll('.email-checkbox-received:checked');
 
